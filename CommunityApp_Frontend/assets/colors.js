20c015ec10612bccadd0168fd8474233ca91b987
FathimaMubarack <165816074+FathimaMubarack@users.noreply.github.com>
2024-11-30 14:01:07 +0400

    Add files via upload

---
+++ CommunityApp_Frontend/assets/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    black: '#000000',
+    gray: '#666666',
+    blue: '#1C215D',
+    fadedBlue: '#8D8FAD',
+    white: '#FFFFFF',
+    darkBlue: '#002244',
+    aliceBlue: '#f0f8ff',
+    lightGrey: '#dcdcdc',
+}
+
+export default colors
